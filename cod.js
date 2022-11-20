ef536b7b4d6eb6e3b6f4a4a8578e350fd051a7ae
Tiago Figueiredo <107004021+FigueiredoTiago@users.noreply.github.com>
2022-11-20 13:02:37 -0300

    Initial commit

---
+++ cod.js
@@ -0,0 +1,36 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const dia = document.getElementById('dia');
+const mes = document.getElementById('mes');
+const ano = document.getElementById('ano');
+
+(function() {
+    setInterval(function() {
+        let data = new Date();
+        let hr = data.getHours();
+        let min =  data.getMinutes();
+        let seg = data.getSeconds();
+
+        let d = data.getDate();
+        let m = data.getMonth() + 1;
+        let a = data.getFullYear();
+
+        if (hr < 10) seg = '0' + hr;
+        if (min <  10) min = '0' + min;
+        if (seg < 10) seg = '0' + seg;
+
+        horas.textContent = hr;
+        minutos.textContent = min;
+        segundos.textContent = seg;
+
+        dia.innerText = d;
+        mes.innerText = m;
+        ano.innerText = a;
+
+
+
+
+    })
+})()
